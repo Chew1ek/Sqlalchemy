86bf938bed624e4b8fc2a98fa41906c4239de327
Namilsky <alive6863@gmail.com>
2025-03-13 19:40:29 +0300

    вроде как подходит для 4 задачи

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from data import db_session
 from data.users import User
+from data.jobs import Jobs
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -42,6 +43,15 @@ def main():
     user.email = "alt_scott_chief@mars.org"
     user.hashed_password = "alt_cap"
     db_sess.add(user)
+    db_sess.commit()
+
+    job = Jobs()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.is_finished = False
+    db_sess.add(job)
     db_sess.commit()
 
 
