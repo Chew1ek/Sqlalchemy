8dff1b1232db5ec16eebde204149b8780c07deb6
Namilsky <alive6863@gmail.com>
2025-03-17 18:53:17 +0300

    задача Запрос 1

--- main.py
+++ main.py
@@ -8,51 +8,15 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
-    db_session.global_init("db/blogs.db")
+    db_session.global_init(input())
     # app.run()
     user = User()
-    user.surname = "Scott"
-    user.name = "Ridley"
-    user.age = 21
-    user.position = "captain"
-    user.speciality = "research engineer"
-    user.address = "module_1"
-    user.email = "scott_chief@mars.org"
-    user.hashed_password = "cap"
+
     db_sess = db_session.create_session()
-    db_sess.add(user)
     db_sess.commit()
 
-    user.surname = "NeScott"
-    user.name = "Ridley"
-    user.age = 21
-    user.position = "captain"
-    user.speciality = "research engineer"
-    user.address = "module_1"
-    user.email = "scott_ne_chief@mars.org"
-    user.hashed_password = "no_cap"
-    db_sess.add(user)
-    db_sess.commit()
-
-    user.surname = "AltScott"
-    user.name = "Ridley"
-    user.age = 21
-    user.position = "captain"
-    user.speciality = "research engineer"
-    user.address = "module_1"
-    user.email = "alt_scott_chief@mars.org"
-    user.hashed_password = "alt_cap"
-    db_sess.add(user)
-    db_sess.commit()
-
-    job = Jobs()
-    job.team_leader = 1
-    job.job = 'deployment of residential modules 1 and 2'
-    job.work_size = 15
-    job.collaborators = '2, 3'
-    job.is_finished = False
-    db_sess.add(job)
-    db_sess.commit()
+    for i in db_sess.query(User).all():
+        print(f'<Colonist> {i.id} {i.surname} {i.name}')
 
 
 if __name__ == '__main__':
